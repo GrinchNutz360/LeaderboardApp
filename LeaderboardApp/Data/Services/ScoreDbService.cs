d4679821a460a2565da8c08e72f480da0609972f
cole-451 <coleneely87@gmail.com>
2026-03-09 15:17:40 -0600

    added commands to interface and db context AND service. this stupid chud has i life, i decree

---
+++ LeaderboardApp/Data/Services/ScoreDbService.cs
@@ -0,0 +1,39 @@
+﻿using LeaderboardApp.Data.Interfaces;
+using LeaderboardApp.Models;
+
+namespace LeaderboardApp.Data.Services
+{
+    public class ScoreDbService : IScoreable
+    {
+        public ScoreDbContext context { get; set; }
+        public void AddScore(Scores score)
+        {
+            context.Scores.Add(score);
+            context.SaveChanges();
+        }
+
+        public IEnumerable<Scores> GetAllScores()
+        {
+            return context.Scores;
+        }
+
+        public IEnumerable<Songs> GetAllSongs()
+        {
+            return context.Songs;
+        }
+
+        public IEnumerable<Users> GetAllUsers()
+        {
+            return context.Users;
+        }
+
+        public Scores? RemoveScore(int id)
+        {
+            Scores selected = GetAllScores().FirstOrDefault(dih => dih.ID == id)!;
+            context.Remove(selected);
+            context.SaveChanges();
+
+            return selected;
+        }
+    }
+}
